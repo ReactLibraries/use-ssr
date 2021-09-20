cbca92e9e4e41b8329e0e9cc00bdbd540cc98113
SoraKumo <info@croud.jp>
2021-09-21 08:14:31 +0900

    fix-test

---
+++ test/index.test.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import { reset } from '@react-libraries/use-global-state';
+import { createCache, getDataFromTree, useSSR } from '../src';
+
+let container: HTMLElement;
+beforeEach(() => {
+  reset();
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+});
+
+it('SSR', async () => {
+  const Component01 = () => {
+    const [count, setCount] = useState(0);
+    const [state, setState] = useSSR<number>('Component01', async (state, setState) => {
+      if (state === 100) return;
+      setState(10);
+      setCount((v) => v + 1);
+      setState(100);
+    });
+    useEffect(() => {
+      setState(200);
+    }, []);
+    return <>{[state, count]}</>;
+  };
+  const Component02 = () => {
+    const [count, setCount] = useState(0);
+    const [state, setState] = useSSR<number>(['Component', '02'], async (state, setState) => {
+      if (state !== undefined) return;
+      setState(10);
+      await new Promise((resolve) => setTimeout(resolve, 1));
+      setState(100);
+      setCount((v) => v + 1);
+    });
+    useEffect(() => {
+      setState(undefined);
+    }, []);
+    return <>{[state, count]}</>;
+  };
+
+  await act(async () => {
+    const cache = await getDataFromTree(<Component01 />);
+    expect(cache).toMatchSnapshot();
+    render(
+      <>
+        <Component01 />
+      </>,
+      container
+    );
+  });
+  expect(container.childNodes).toMatchSnapshot();
+
+  unmountComponentAtNode(container);
+  container.remove();
+
+  await act(async () => {
+    const cache = await getDataFromTree(<Component01 />);
+    createCache(cache);
+    render(
+      <>
+        <Component01 />
+      </>,
+      container
+    );
+  });
+  expect(container.childNodes).toMatchSnapshot();
+
+  unmountComponentAtNode(container);
+  container.remove();
+
+  await act(async () => {
+    const cache = await getDataFromTree(<></>);
+    expect(cache).toMatchSnapshot();
+    createCache(cache);
+    render(
+      <>
+        <Component01 />
+        <Component02 />
+      </>,
+      container
+    );
+    await new Promise((r) => setTimeout(r, 100));
+  });
+  expect(container.childNodes).toMatchSnapshot();
+  unmountComponentAtNode(container);
+  container.remove();
+
+  await act(async () => {
+    const cache = await getDataFromTree(
+      <>
+        <Component01 />
+        <Component02 />
+      </>
+    );
+    expect(cache).toMatchSnapshot();
+    createCache(cache);
+    render(
+      <>
+        <Component01 />
+        <Component02 />
+      </>,
+      container
+    );
+    await new Promise((r) => setTimeout(r, 100));
+  });
+
+  expect(container.childNodes).toMatchSnapshot();
+});
